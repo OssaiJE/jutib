552c88537c0d6aad6d85520b01b8c836049bf56b
OssaiJE <ossaije1@gmail.com>
2021-11-25 19:41:49 +0100

    updated ecommerce API

---
+++ index.js
@@ -0,0 +1,51 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+// import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+
+import productRoutes from "./routes/product.routes.js";
+import userRoutes from "./routes/user.routes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+
+//  Load config file
+dotenv.config({ path: "./config/config.env" });
+
+//  Connect to database
+connectDB();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// Morgan logs request to console
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+// app.use("/api/orders", orderRoutes);
+// app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 3031;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
